ed83d780d38f5a11584f9a6b511a210c4b3c2897
caizh <caizh@caizhh>
2018-09-11 18:02:29 +0800

    代码review调整

--- src/main/java/com/moseeker/dto/ResultObject.java
+++ src/main/java/com/moseeker/dto/ResultObject.java
@@ -1,13 +1,22 @@
 package com.moseeker.dto;
 
+import com.moseeker.enums.CommonExceptionEnum;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResultObject<T> {
 	
 	private String code;
 	private String message;
 	private T data;
 
-
+	public static <T> ResultObject<T> getSuccessResultObject(T object) {
+		return new ResultObject<T>(CommonExceptionEnum.success.getKey(), CommonExceptionEnum.success.getValue(), object);
+	}
+	
 }
