6cfec72727d63558731b84ec232932b53c9e82b5
刘旭辉 <liuxuhui@moseeker.com>
2018-09-12 10:05:45 +0800

    dict redis

--- src/main/java/com/moseeker/dto/Result.java
+++ src/main/java/com/moseeker/dto/Result.java
@@ -8,4 +8,19 @@ public class Result {
 	private String code;
 	private String message;
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
