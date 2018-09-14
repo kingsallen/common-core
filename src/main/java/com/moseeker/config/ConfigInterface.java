3eefba348932111969d6dc3450398e5cc40f2588
caizhaohui <caizhaohui@moseeker.com>
2018-09-14 19:42:49 +0800

    facade的filter逻辑迁移到core

--- src/main/java/com/moseeker/config/ConfigInterface.java
+++ src/main/java/com/moseeker/config/ConfigInterface.java
@@ -1,6 +1,7 @@
 package com.moseeker.config;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,8 @@ import lombok.Data;
 @Component
 @Data
 public class ConfigInterface {
-	
+
 	private List<String> list;
+	private Map<String, String> map;
 
 }
