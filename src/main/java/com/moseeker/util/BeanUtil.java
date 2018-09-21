d59b9354edbcabe570fce37cd3efc62a160e2d3c
caizhaohui <caizhaohui@moseeker.com>
2018-09-21 11:30:28 +0800

    BeanUtil修改

--- src/main/java/com/moseeker/util/BeanUtil.java
+++ src/main/java/com/moseeker/util/BeanUtil.java
@@ -11,6 +11,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.moseeker.vo.Pagination;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.FatalBeanException;
@@ -23,12 +25,135 @@ import com.moseeker.exception.BaseException;
  */
 public final class BeanUtil extends BeanUtils {
 
+	public static <T> void copy(Pagination<T> source, Pagination<T> target) throws BeansException, IllegalAccessException, InstantiationException {
+		copy(source, target, null, null);
+	}
+
+	public static <T> void copy(Pagination<T> source, Pagination<T> target, String[] ignoreProperties) throws BeansException, IllegalAccessException, InstantiationException {
+		copy(source, target, ignoreProperties, null);
+	}
+
+	public static <T> void copy(Pagination<T> source, Pagination<T> target, String[] ignoreProperties, String[] nullProperties) throws BeansException, InstantiationException, IllegalAccessException {
+		if (source == null) {
+			throw new BaseException(C.EMPTY, "参数source不能为null");
+		}
+		if (target == null) {
+			target = new Pagination<>();
+		}
+		target.setPagination(source);
+		if (source.getResults() != null) {
+			copy(source.getResults(), target.getResults(), ignoreProperties, nullProperties);
+		}
+	}
+
+	public static <K, T> void copy(Map<K, T> source, Map<K, T> target) throws BeansException, InstantiationException, IllegalAccessException {
+		copy(source, target, null, null);
+	}
+
+	public static <K, T> void copy(Map<K, T> source, Map<K, T> target, String[] ignoreProperties) throws BeansException, InstantiationException, IllegalAccessException {
+		copy(source, target, ignoreProperties, null);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <K, T> void copy(Map<K, T> source, Map<K, T> target, String[] ignoreProperties, String[] nullProperties) throws BeansException, InstantiationException, IllegalAccessException {
+		if (source == null) {
+			throw new BaseException(C.EMPTY, "参数source不能为null");
+		}
+		if (target == null) {
+			target = new HashMap<>();
+		}
+		target.clear();
+		for (K k : source.keySet()) {
+			T s = source.get(k);
+			T t = (T) s.getClass().newInstance();
+			copy(s, t, ignoreProperties, nullProperties);
+			target.put(k, t);
+		}
+	}
+
+	public static <T> void copy(List<T> source, List<T> target) throws BeansException, InstantiationException, IllegalAccessException {
+		copy(source, target, null, null);
+	}
+
+	public static <T> void copy(List<T> source, List<T> target, String[] ignoreProperties) throws BeansException, InstantiationException, IllegalAccessException {
+		copy(source, target, ignoreProperties, null);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> void copy(List<T> source, List<T> target, String[] ignoreProperties, String[] nullProperties) throws BeansException, IllegalAccessException, InstantiationException {
+		if (source == null) {
+			throw new BaseException(C.EMPTY, "参数source不能为null");
+		}
+		if (target == null) {
+			target = new ArrayList<>();
+		}
+		target.clear();
+		for (T s : source) {
+			T t = (T) s.getClass().newInstance();
+			copy(s, t, ignoreProperties, nullProperties);
+			target.add(t);
+		}
+	}
+
+	public static <T> void copy(T source, T target) throws BeansException, InstantiationException, IllegalAccessException {
+		copy(source, target, null, null);
+	}
+
+	public static <T> void copy(T source, T target, String[] ignoreProperties) throws BeansException, InstantiationException, IllegalAccessException {
+		copy(source, target, ignoreProperties, null);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> void copy(T source, T target, String[] ignoreProperties, String[] nullProperties) throws BeansException, InstantiationException, IllegalAccessException {
+		if (source == null) {
+			throw new BaseException(C.EMPTY, "参数source不能为null");
+		}
+		if (target == null) {
+			target = (T) source.getClass().newInstance();
+		}
+		Class<?> actualEditable = target.getClass();
+		PropertyDescriptor[] targetPropertyDescriptors = BeanUtils.getPropertyDescriptors(actualEditable);
+		List<String> ignoreList = (ignoreProperties != null) ? Arrays.asList(ignoreProperties) : null;
+		List<String> nullPropertiesList = (nullProperties != null) ? Arrays.asList(nullProperties) : new ArrayList<>();
+		for (PropertyDescriptor targetPropertyDescriptor : targetPropertyDescriptors) {
+			if (targetPropertyDescriptor.getWriteMethod() != null && (ignoreProperties == null || (!ignoreList.contains(targetPropertyDescriptor.getName())))) {
+				PropertyDescriptor sourcePropertyDescriptor = BeanUtils.getPropertyDescriptor(source.getClass(), targetPropertyDescriptor.getName());
+				if (sourcePropertyDescriptor != null && sourcePropertyDescriptor.getReadMethod() != null) {
+					try {
+						Method readMethod = sourcePropertyDescriptor.getReadMethod();
+						if (!Modifier.isPublic(readMethod.getDeclaringClass().getModifiers())) {
+							readMethod.setAccessible(true);
+						}
+						Object value = readMethod.invoke(source);
+						Method targetReadMethod = targetPropertyDescriptor.getReadMethod();
+						if (!Modifier.isPublic(targetReadMethod.getDeclaringClass().getModifiers())) {
+							targetReadMethod.setAccessible(true);
+						}
+						Object targetVal = targetReadMethod.invoke(target);
+						if (targetVal != null && value == null && !nullPropertiesList.contains(targetPropertyDescriptor.getName())) {
+							continue;
+						}
+						Method writeMethod = targetPropertyDescriptor.getWriteMethod();
+						if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
+							writeMethod.setAccessible(true);
+						}
+						writeMethod.invoke(target, value);
+					} catch (Throwable e) {
+						throw new FatalBeanException("Could not copy properties from source to target", e);
+					}
+				}
+			}
+		}
+	}
+
 	/**
 	 * @param source
 	 * @param target
 	 * @throws BeansException
+	 * @Deprecated 用copy方法替代
 	 */
-	public static final void copyProperties(Object source, Object target) throws BeansException {
+	@Deprecated
+	public static void copyProperties(Object source, Object target) throws BeansException {
 		copyProperties(source, target, null, null);
 	}
 
@@ -37,8 +162,10 @@ public final class BeanUtil extends BeanUtils {
 	 * @param target
 	 * @param ignoreProperties
 	 * @throws BeansException
+	 * @Deprecated 用copy方法替代
 	 */
-	public static final void copyProperties(Object source, Object target, String[] ignoreProperties) throws BeansException {
+	@Deprecated
+	public static void copyProperties(Object source, Object target, String[] ignoreProperties) throws BeansException {
 		copyProperties(source, target, ignoreProperties, null);
 	}
 
@@ -48,8 +175,10 @@ public final class BeanUtil extends BeanUtils {
 	 * @param ignoreProperties
 	 * @param nullProperties
 	 * @throws BeansException
+	 * @Deprecated 用copy方法替代
 	 */
-	public static final void copyProperties(Object source, Object target, String[] ignoreProperties, String[] nullProperties) throws BeansException {
+	@Deprecated
+	public static void copyProperties(Object source, Object target, String[] ignoreProperties, String[] nullProperties) throws BeansException {
 		if (source == null) {
 			throw new BaseException(C.EMPTY, "Source must not be null");
 		}
@@ -89,10 +218,9 @@ public final class BeanUtil extends BeanUtils {
 				}
 			}
 		}
-
 	}
 
-	public static final void setPropertyVal(Object bean, String propertyName, Object propertyValue) throws Throwable {
+	public static void setPropertyVal(Object bean, String propertyName, Object propertyValue) throws Throwable {
 		if (bean == null) {
 			return;
 		}
@@ -102,38 +230,30 @@ public final class BeanUtil extends BeanUtils {
 		PropertyDescriptor propertyDescriptor = BeanUtils.getPropertyDescriptor(bean.getClass(), propertyName);
 		if (propertyDescriptor != null) {
 			if (propertyDescriptor.getReadMethod() != null) {
-				try {
-					Method writeMethod = propertyDescriptor.getWriteMethod();
-					if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
-						writeMethod.setAccessible(true);
-					}
-					writeMethod.invoke(bean, propertyValue);
-				} catch (Throwable e) {
-					throw e;
+				Method writeMethod = propertyDescriptor.getWriteMethod();
+				if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
+					writeMethod.setAccessible(true);
 				}
+				writeMethod.invoke(bean, propertyValue);
 			}
 		}
 	}
 
-	public static final Map<String, Object> transBean2Map(Object obj) throws Exception {
+	public static Map<String, Object> transBean2Map(Object obj) throws Exception {
 		if (obj == null) {
 			return null;
 		}
-		Map<String, Object> map = new HashMap<String, Object>();
-		try {
-			BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
-			PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
-			for (PropertyDescriptor property : propertyDescriptors) {
-				String key = property.getName();
-				// 过滤class属性
-				if (!key.equals("class")) {// 得到property对应的getter方法
-					Method getter = property.getReadMethod();
-					Object value = getter.invoke(obj);
-					map.put(key, value);
-				}
+		Map<String, Object> map = new HashMap<>();
+		BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+		PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+		for (PropertyDescriptor property : propertyDescriptors) {
+			String key = property.getName();
+			// 过滤class属性
+			if (!key.equals("class")) {// 得到property对应的getter方法
+				Method getter = property.getReadMethod();
+				Object value = getter.invoke(obj);
+				map.put(key, value);
 			}
-		} catch (Exception e) {
-			throw e;
 		}
 		return map;
 	}
