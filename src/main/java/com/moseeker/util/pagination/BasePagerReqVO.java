d9ce384f41da2911e3ee76a628d86c279b5f7810
wangyaofeng <wangyaofeng@moseeker.com>
2018-11-12 18:17:16 +0800

    分页框架

---
+++ src/main/java/com/moseeker/util/pagination/BasePagerReqVO.java
@@ -0,0 +1,17 @@
+package com.moseeker.util.pagination;
+
+import com.moseeker.util.C;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class BasePagerReqVO<Query> {
+    @ApiModelProperty(value = "每页数量",required = true)
+    protected Integer pageSize;
+
+    @ApiModelProperty(value = "请求第几页",required = true)
+    protected Integer currentPage;
+
+    @ApiModelProperty(value = "搜索条件"，required = false)
+    Query query;
+}
