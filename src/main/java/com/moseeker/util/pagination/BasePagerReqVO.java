92b0fbd139b5e883b3d50d8548ea990318ddc019
wangyaofeng <wangyaofeng@moseeker.com>
2018-11-13 11:57:50 +0800

    query.param1

--- src/main/java/com/moseeker/util/pagination/BasePagerReqVO.java
+++ src/main/java/com/moseeker/util/pagination/BasePagerReqVO.java
@@ -1,16 +1,14 @@
 package com.moseeker.util.pagination;
 
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-public class BasePagerReqVO<Query> {
+public class BasePagerReqVO {
     @ApiModelProperty(value = "每页数量",required = true)
     protected Integer pageSize;
 
     @ApiModelProperty(value = "请求第几页",required = true)
     protected Integer currentPage;
-
-    @ApiModelProperty(value = "搜索条件",required = false)
-    Query query;
 }
