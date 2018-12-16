abc1b990add4a2aa5ad209518aa306cb13da8018
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-12-16 10:26:59 +0800

    FloatFormatter 增加除100之后再格式化的方法。 由于我们的金额展示都是元，但是存储是分，所以需要做对应的格式化

--- src/main/java/com/moseeker/util/FloatFormatter.java
+++ src/main/java/com/moseeker/util/FloatFormatter.java
@@ -14,6 +14,13 @@ public class FloatFormatter {
     private static DecimalFormat bonus1Format = new DecimalFormat("###################.#");
     private static DecimalFormat bonus2Format = new DecimalFormat("###################.##");
 
+    /**
+     * 小数点展示规则
+     * 只显示小数点后两位有效数值
+     * 如果小数点后最后一位为0则不展示
+     * @param bonus 被格式化的数值
+     * @return 格式化后的数值
+     */
     public static String toString(double bonus) {
         if (bonus - (int)bonus == 0) {
             return bonusFormat.format(bonus);
@@ -22,5 +29,15 @@ public class FloatFormatter {
         } else {
             return bonus2Format.format(bonus);
         }
+    }
+
+    /**
+     * 十进制向左移动两位再格式化
+     * @param bonus 被格式化的数值
+     * @return 格式化后的数值
+     */
+    public static String dive100(double bonus) {
+        bonus = bonus / 100;
+        return toString(bonus);
     }
 }
