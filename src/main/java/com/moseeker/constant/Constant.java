c02b565125f6a8fe69266af0d2a10ea4fd007ae7
刘旭辉 <liuxuhui@moseeker.com>
2019-02-27 11:34:16 +0800

    kafka

--- src/main/java/com/moseeker/constant/Constant.java
+++ src/main/java/com/moseeker/constant/Constant.java
@@ -396,5 +396,7 @@ public final class Constant {
 
     public static final String POSITION_SYNC_FAIL_ROUTINGKEY = "messagetemplate.position_sync_fail";
 
+    public static final String KAFKA_TOPIC_POSITION_STATUS = "radar_position_status";
+
 
 }
