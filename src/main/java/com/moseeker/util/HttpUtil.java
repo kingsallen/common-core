9e54859c43de1fcf75a51d05fc0f6874392dc0be
panyongbing <panyongbing@moseeker.com>
2019-03-20 09:56:54 +0800

    面试通知

--- src/main/java/com/moseeker/util/HttpUtil.java
+++ src/main/java/com/moseeker/util/HttpUtil.java
@@ -131,6 +131,57 @@ public class HttpUtil {
 		return result.toString();
 	}
 
+	public static String sendPost(String url, String param) throws ConnectException {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			// 设置通用的请求属性
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("Content-type","application/json");
+			// 发送POST请求必须设置如下两行
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setConnectTimeout(25000);
+			conn.setReadTimeout(25000);
+			// 获取URLConnection对象对应的输出流
+			out = new PrintWriter(conn.getOutputStream());
+			// 发送请求参数
+			out.print(param);
+			// flush输出流的缓冲
+			out.flush();
+			// 定义BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			throw new ConnectException();
+		}
+		//使用finally块来关闭输出流、输入流
+		finally{
+			try{
+				if(out!=null){
+					out.close();
+				}
+				if(in!=null){
+					in.close();
+				}
+			}
+			catch(IOException ex){
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+
 	public static final JSONObject sendHttpsRequest(String requestUrl, String requestMethod, String param) throws Exception {
 		if (StringUtils.isEmpty(requestUrl)) {
 			throw new BaseException(Constant.EMPTY, "requestUrl不能为空");
