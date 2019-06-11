e64ff4f19eb2ea019ca9f79fc527e6a9a9713b09
李成飞 <lichengfei@moseeker.com>
2019-06-11 11:59:16 +0800

    简历来源内推改为内推上传

--- src/main/java/com/moseeker/constant/profile/ProfileOriginEnum.java
+++ src/main/java/com/moseeker/constant/profile/ProfileOriginEnum.java
@@ -17,7 +17,7 @@ public enum ProfileOriginEnum {
     ORIGIN_MINHANG(27,"民航上传"),
     ORIGIN_NEW_GRADUATION(28,"应届生（上传）"),
     ORIGIN_EMPLOYEE_SEND(29,"员工主动上传"),
-    ORIGIN_EMPLOYEE_RECOM(30,"内推"),
+    ORIGIN_EMPLOYEE_RECOM(30,"内推（上传）"),
     ORIGIN_51_SEND(31,"前程无忧（主投导入）"),
     ORIGIN_51_DOWNLOAD(32,"前程无忧（下载导入）"),
     ORIGIN_ZHILIAN_SEND(33,"智联招聘（主投导入）"),
