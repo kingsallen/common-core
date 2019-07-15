9176b1ed5619b7ec809bec4a745facafad92fc61
chenhan <chenhan@moseeker.com>
2019-07-15 10:44:12 +0800

    zipkin收集自定义traceid

--- src/main/java/com/moseeker/configuration/TraceInterceptor.java
+++ src/main/java/com/moseeker/configuration/TraceInterceptor.java
@@ -3,6 +3,10 @@ package com.moseeker.configuration;
 import com.moseeker.constant.TraceConstant;
 import com.moseeker.util.StringUtils;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +20,9 @@ public class TraceInterceptor implements HandlerInterceptor {
 
     protected String uuid = "";
 
+    @Autowired
+    BeanFactory factory;
+
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         uuid = httpServletRequest.getHeader(TraceConstant.HEADERNAME);
@@ -23,6 +30,16 @@ public class TraceInterceptor implements HandlerInterceptor {
             uuid = UUID.randomUUID().toString();
         }
         MDC.put(TraceConstant.CUSTOM_TRACE_ID, uuid);
+        if (factory != null) {
+            Tracer tracer = factory.getBean(Tracer.class);
+            if (tracer != null) {
+                Span span = tracer.getCurrentSpan();
+                if (span != null) {
+                    span.tag(TraceConstant.CUSTOM_Tag, uuid);
+                }
+            }
+
+        }
         return true;
     }
 
--- src/main/java/com/moseeker/constant/TraceConstant.java
+++ src/main/java/com/moseeker/constant/TraceConstant.java
@@ -5,4 +5,5 @@ public class TraceConstant {
     public static final String SLEUTHHEADERNAME = "sleuth-traceId";
     public static final String CUSTOM_TRACE_ID = "customTraceId";
     public static final String X_B3_TRACE_ID = "X-B3-TraceId";
+    public static final String CUSTOM_Tag="Custom-TraceId";
 }
