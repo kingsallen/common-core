c2837adbbd686c9e5be808ff7638bbb5c245d276
zhangzeteng@moseeker.com <zhangzeteng@moseeker.com>
2019-07-17 11:51:21 +0800

    add

--- src/main/java/com/moseeker/constant/RabbitMQConstant.java
+++ src/main/java/com/moseeker/constant/RabbitMQConstant.java
@@ -30,8 +30,10 @@ public enum RabbitMQConstant {
     HR_INTERVIEW_CANCEL_NOTICE_ROUTEKEY("hr_interview_cancel_notice_routekey"),
     POSITION_QUEUE_UPDATE_PUBLISHER("position_queue_update_publisher"),
     POSITION_QUEUE_UPDATE_PUBLISHER_ROUTEKEY("position_queue_update_publisher_routekey"),
-    POSITION_QUEUE_UPDATE_PUBLISHER_EXCHANGE("position_queue_update_publisher_exchange")
-
+    POSITION_QUEUE_UPDATE_PUBLISHER_EXCHANGE("position_queue_update_publisher_exchange"),
+    NEW_PROCESS_ATS_MESSSAGE_QUEUE("new_process_ats_message_queue"),
+    NEW_PROCESS_ATS_MESSSAGE_EXCHANGE("new_process_ats_message_exchage"),
+    NEW_PROCESS_ATS_MESSSAGE_ROUTINGKEY("new_process_ats_message_routingkey")
     ;
 
     private String value;
