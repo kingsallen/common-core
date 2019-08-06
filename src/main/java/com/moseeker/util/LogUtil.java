7555d542b6a8add7d71412ac7bb489b38574c056
chenhan <chenhan@moseeker.com>
2019-08-06 11:45:54 +0800

    写日志

--- src/main/java/com/moseeker/util/LogUtil.java
+++ src/main/java/com/moseeker/util/LogUtil.java
@@ -1,7 +1,6 @@
 package com.moseeker.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.alibaba.fastjson.JSON;
 import com.moseeker.constant.LogType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,22 +9,21 @@ import java.util.Map;
 
 public class LogUtil {
     private final static Logger logger = LoggerFactory.getLogger(LogUtil.class);
-    private final static ObjectMapper mapper = new ObjectMapper();
 
-    private static void debug(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) throws JsonProcessingException {
-        logger.debug("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, mapper.writeValueAsString(extraInfo));
+    private static void debug(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) {
+        logger.debug("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, JSON.toJSONString(extraInfo));
     }
 
-    private static void info(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) throws JsonProcessingException {
-        logger.info("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, mapper.writeValueAsString(extraInfo));
+    private static void info(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) {
+        logger.info("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, JSON.toJSONString(extraInfo));
     }
 
-    private static void warn(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) throws JsonProcessingException {
-        logger.warn("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, mapper.writeValueAsString(extraInfo));
+    private static void warn(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) {
+        logger.warn("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, JSON.toJSONString(extraInfo));
     }
 
-    private static void error(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) throws JsonProcessingException {
-        logger.error("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, mapper.writeValueAsString(extraInfo));
+    private static void error(String serviceName, String methodName, String msg, String filter1, String filter2, Map<String, Object> extraInfo) {
+        logger.error("[{}][{}][{}][{}][{}][{}]", serviceName, methodName, filter1, filter2, msg, JSON.toJSONString(extraInfo));
     }
 
     public static boolean CommonLog(LogType logType, String serviceName, String methodName, String message, String filter1, String filter2, Map<String, Object> extraInfo) {
