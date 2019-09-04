994bc90b54253cc884e41c8a93d7f61b4e8d0a69
吴雷 <wulei@moseeker.com>
2019-09-04 13:52:13 +0800

    revert some code

--- src/test/java/com/moseeker/AppTest.java
+++ src/test/java/com/moseeker/AppTest.java
@@ -1,19 +1,7 @@
 package com.moseeker;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.io.File;
-import java.io.FileOutputStream;
-
 public class AppTest {
 	
-	public static void main(String[] args) throws Exception{
-
-	}
-
-	@Test
-	public void test00(){
-		Assert.assertEquals(true,Integer.valueOf("2131").equals(Integer.valueOf("2131")));
+	public static void main(String[] args) {
 	}
 }
