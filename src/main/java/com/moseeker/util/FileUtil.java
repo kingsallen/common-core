104d976b6b31e559ba247fffa6d69dda7e14062e
吴雷 <wulei@moseeker.com>
2019-09-10 14:37:31 +0800

    添加水印工具

--- src/main/java/com/moseeker/util/FileUtil.java
+++ src/main/java/com/moseeker/util/FileUtil.java
@@ -11,10 +11,10 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 /**
  * 图片压缩Utils
@@ -77,6 +77,73 @@ public class FileUtil {
             accuracy = 0.4;
         }
         return accuracy;
+    }
+
+    public static int getWatermarkLength(String waterMarkContent, Graphics2D g) {
+        return g.getFontMetrics(g.getFont()).charsWidth(waterMarkContent.toCharArray(), 0, waterMarkContent.length());
+    }
+
+    public static void main(String[] args) {
+        Font font = new Font("微软雅黑", Font.PLAIN, 50);                     //水印字体
+        String srcImgPath="/Users/wulei/Desktop/归档/Rays.jpeg"; //源图片地址
+        String tarImgPath="/Users/wulei/Desktop/归档/Rays.jpeg"; //待存储的地址
+        String waterMarkContent="此照片仅供招聘使用";  //水印内容
+        Color color=new Color(255,255,255,128);                               //水印图片色彩以及透明度
+        new FileUtil().mark(srcImgPath, tarImgPath, waterMarkContent,font,color);
+    }
+
+    /**
+     * 给图片增加文字水印
+     *
+     * @param imgPath
+     *            -要添加水印的图片路径
+     * @param outImgPath
+     *            -输出路径
+     * @param text-文字
+     * @param font
+     *            -字体
+     * @param color
+     *            -颜色
+     */
+    public static void mark(String imgPath, String outImgPath, String text, Font font, Color color) {
+        try {
+            // 读取原图片信息
+            File imgFile = null;
+            Image img = null;
+            if (imgPath != null) {
+                imgFile = new File(imgPath);
+            }
+            if (imgFile != null && imgFile.exists() && imgFile.isFile() && imgFile.canRead()) {
+                img = ImageIO.read(imgFile);
+            }
+            int imgWidth = img.getWidth(null);
+            int imgHeight = img.getHeight(null);
+            // 加水印
+            BufferedImage bufImg = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB);
+            mark(bufImg, img, text, font, color);
+            // 输出图片
+            FileOutputStream outImgStream = new FileOutputStream(outImgPath);
+            ImageIO.write(bufImg, "jpg", outImgStream);
+            outImgStream.flush();
+            outImgStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // 加文字水印
+    public static void mark(BufferedImage bufImg, Image img, String text, Font font, Color color) {
+        Graphics2D g = bufImg.createGraphics();
+        int x = (int) (bufImg.getWidth()-getWatermarkLength(text,g)-(font.getSize()*text.length()));;
+        int y = (int) (bufImg.getHeight()*0.96);;
+        //设置水印的坐标
+        g.drawImage(img, 0, 0, bufImg.getWidth(), bufImg.getHeight(), null);
+        g.setColor(color);
+        g.setFont(font);
+        x = x<0?0:x;
+        g.drawString(text, x, y);
+        g.dispose();
     }
 
 }
