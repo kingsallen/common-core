13fc1123aebe4e3fbdb9941879205e45d1e61745
吴雷 <wulei@moseeker.com>
2019-09-18 11:51:19 +0800

    注释上传文件大小配置

--- src/main/java/com/moseeker/config/ConfigFileUpload.java
+++ src/main/java/com/moseeker/config/ConfigFileUpload.java
@@ -14,15 +14,15 @@ import javax.servlet.MultipartConfigElement;
 @Configuration
 public class ConfigFileUpload {
 
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        //  单个数据大小
-        factory.setMaxFileSize("10240KB");
-        /// 总上传数据大小
-        factory.setMaxRequestSize("10240KB");
-        return factory.createMultipartConfig();
-    }
+//    @Bean
+//    public MultipartConfigElement multipartConfigElement() {
+//        MultipartConfigFactory factory = new MultipartConfigFactory();
+//        //  单个数据大小
+//        factory.setMaxFileSize("10240KB");
+//        /// 总上传数据大小
+//        factory.setMaxRequestSize("10240KB");
+//        return factory.createMultipartConfig();
+//    }
 
 
 }
