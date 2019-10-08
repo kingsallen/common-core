ad7979d35540c30164193b3f75197f8ba7e8b869
chenhan <chenhan@moseeker.com>
2019-10-08 11:26:08 +0800

    log改成json格式

--- src/main/java/com/moseeker/configuration/MyJsonLayout.java
+++ src/main/java/com/moseeker/configuration/MyJsonLayout.java
@@ -73,15 +73,15 @@ public class MyJsonLayout extends LayoutBase<ILoggingEvent> {
                                 Map<String, String> mdc) {
         if (value != null) {
             buf.append(DBL_QUOTE);
-            buf.append(escape(key));
+            buf.append(key);
             buf.append(DBL_QUOTE);
             buf.append(':');
             buf.append(DBL_QUOTE);
-            buf.append(escape(mdcSubst(value, mdc)));
+            buf.append(mdcSubst(value, mdc));
             buf.append(DBL_QUOTE);
         } else {
             buf.append(DBL_QUOTE);
-            buf.append(escape(key));
+            buf.append(key);
             buf.append(DBL_QUOTE);
             buf.append(':');
             buf.append("null");
@@ -103,53 +103,6 @@ public class MyJsonLayout extends LayoutBase<ILoggingEvent> {
             return sb.toString();
         }
         return v;
-    }
-
-    private String escape(String s) {
-        if (s == null)
-            return null;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            switch (ch) {
-                case '"':
-                    sb.append("\\\"");
-                    break;
-                case '\\':
-                    sb.append("\\\\");
-                    break;
-                case '\b':
-                    sb.append("\\b");
-                    break;
-                case '\f':
-                    sb.append("\\f");
-                    break;
-                case '\n':
-                    sb.append("\\n");
-                    break;
-                case '\r':
-                    sb.append("\\r");
-                    break;
-                case '\t':
-                    sb.append("\\t");
-                    break;
-                case '/':
-                    sb.append("\\/");
-                    break;
-                default:
-                    if (ch >= '\u0000' && ch <= '\u001F') {
-                        String ss = Integer.toHexString(ch);
-                        sb.append("\\u");
-                        for (int k = 0; k < 4 - ss.length(); k++) {
-                            sb.append('0');
-                        }
-                        sb.append(ss.toUpperCase());
-                    } else {
-                        sb.append(ch);
-                    }
-            }
-        }
-        return sb.toString();
     }
 
     @Override
