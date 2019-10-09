cb5e7162aa719ef98f05de805448082360c59c0b
chenhan <chenhan@moseeker.com>
2019-10-09 16:54:27 +0800

    json格式调整

--- src/main/java/com/moseeker/configuration/MyJsonLayout.java
+++ src/main/java/com/moseeker/configuration/MyJsonLayout.java
@@ -6,6 +6,7 @@ import ch.qos.logback.classic.spi.ThrowableProxyUtil;
 import ch.qos.logback.core.LayoutBase;
 import com.alibaba.fastjson.JSON;
 import com.cwbase.logback.AdditionalField;
+import org.springframework.util.StringUtils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -15,14 +16,10 @@ import java.util.regex.Pattern;
 
 public class MyJsonLayout extends LayoutBase<ILoggingEvent> {
 
-    private final int DEFAULT_SIZE = 256;
-    private final int UPPER_LIMIT = 2048;
-    private final static char DBL_QUOTE = '"';
-    private final static char COMMA = ',';
-
-    //private Map<String,Object> map = new HashMap<>();
     private DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SZ");
     private Pattern MDC_VAR_PATTERN = Pattern.compile("\\@\\{([^}]*)\\}");
+
+    private String NUMBER_REGEX = "\\d+";
 
     List<AdditionalField> additionalFields;
 
@@ -36,7 +33,7 @@ public class MyJsonLayout extends LayoutBase<ILoggingEvent> {
     @Override
     public synchronized String doLayout(ILoggingEvent event) {
         Map<String, String> mdc = event.getMDCPropertyMap();
-        Map<String,Object> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("@timestamp", df.format(new Date(event.getTimeStamp())));
         map.put("level", event.getLevel().toString());
         map.put("thread", event.getThreadName());
@@ -47,33 +44,21 @@ public class MyJsonLayout extends LayoutBase<ILoggingEvent> {
             String throwable = ThrowableProxyUtil.asString(tp);
             map.put("throwable", throwable);
         }
-        if(additionalFields != null) {
-            for(AdditionalField field : additionalFields) {
-                map.put(field.getKey(),mdcSubst(field.getValue(),mdc));
+        if (additionalFields != null) {
+            for (AdditionalField field : additionalFields) {
+                String value = mdcSubst(field.getValue(), mdc);
+                if(StringUtils.isEmpty(value)){
+                    continue;
+                }
+                if (value.matches(NUMBER_REGEX)) {
+                    map.put(field.getKey(), Integer.parseInt(value));
+                } else {
+                    map.put(field.getKey(), value);
+                }
             }
         }
         return JSON.toJSONString(map);
     }
-
-
-//    private void appendKeyValue(StringBuilder buf, String key, String value,
-//                                Map<String, String> mdc) {
-//        if (value != null) {
-//            buf.append(DBL_QUOTE);
-//            buf.append(escape(key));
-//            buf.append(DBL_QUOTE);
-//            buf.append(':');
-//            buf.append(DBL_QUOTE);
-//            buf.append(escape(mdcSubst(value, mdc)));
-//            buf.append(DBL_QUOTE);
-//        } else {
-//            buf.append(DBL_QUOTE);
-//            buf.append(escape(key));
-//            buf.append(DBL_QUOTE);
-//            buf.append(':');
-//            buf.append("null");
-//        }
-//    }
 
     private String mdcSubst(String v, Map<String, String> mdc) {
         if (mdc != null && v != null && v.contains("@{")) {
@@ -96,52 +81,4 @@ public class MyJsonLayout extends LayoutBase<ILoggingEvent> {
     public String getContentType() {
         return "application/json";
     }
-
-//    private String escape(String s) {
-//        if (s == null)
-//            return null;
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 0; i < s.length(); i++) {
-//            char ch = s.charAt(i);
-//            switch (ch) {
-//                case '"':
-//                    sb.append("\\\"");
-//                    break;
-////                case '\\':
-////                    sb.append("\\\\");
-////                    break;
-////                case '\b':
-////                    sb.append("\\b");
-////                    break;
-////                case '\f':
-////                    sb.append("\\f");
-////                    break;
-////                case '\n':
-////                    sb.append("\\n");
-////                    break;
-////                case '\r':
-////                    sb.append("\\r");
-////                    break;
-////                case '\t':
-////                    sb.append("\\t");
-////                    break;
-////                case '/':
-////                    sb.append("\\/");
-////                    break;
-//                default:
-//                    if (ch >= '\u0000' && ch <= '\u001F') {
-//                        String ss = Integer.toHexString(ch);
-//                        sb.append("\\u");
-//                        for (int k = 0; k < 4 - ss.length(); k++) {
-//                            sb.append('0');
-//                        }
-//                        sb.append(ss.toUpperCase());
-//                    } else {
-//                        sb.append(ch);
-//                    }
-//            }
-//        }// for
-//        return sb.toString();
-//    }
-
 }
